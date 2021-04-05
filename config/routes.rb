1e56ee8be2ba302a387bceec256f517a42f2a9a6
AnnieB <anniebellefleur@gmail.com>
2021-04-05 11:50:46 -0400

    Add functionality for current user to sign in

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
 
 # GET /about url and send request to the about controller and the index action
   get "about", to: "about#index"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
 end
